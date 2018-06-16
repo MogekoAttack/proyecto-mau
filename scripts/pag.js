f9118ee7453764041c35ac2a3d1ee03ff454bafd
Miguel Angel <40309748+MogekoAttack@users.noreply.github.com>
2018-06-15 22:17:18 -0500

    Add files via upload

---
+++ scripts/pag.js
@@ -0,0 +1,12 @@
+function pag_1 (url){
+    window.open(url, "Captuar CPU", "width=401, height=401").moveTo(0,0).resizeTo(401,401);
+    }
+function pag_2 (url){
+    window.open(url, "Registrarse", "width=400, height=400");
+    }
+function pag_3(url){
+window.open(url, "Capturar GPU", "width=400, height=400");
+    }
+function pag_4(url){
+    window.open(url, "Nuevo usuario", "width=400, height=400");
+}
